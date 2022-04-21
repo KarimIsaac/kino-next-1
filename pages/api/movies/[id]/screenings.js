2e95c924a5c0a8173f2aade515b4dcce527ed50c
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-04-21 19:01:26 +0200

    create movie id screenings route

---
+++ pages/api/movies/[id]/screenings.js
@@ -0,0 +1,23 @@
+import connectDb from "../../../../DB/connectDb";
+import Screenings from "../../../../DB/models/screenings";
+
+// Get all screenings with same ID as the movie id from /api/movie/[id]/screenings.
+export default async function handler(req, res) {
+    try {
+        connectDb().then(async () => {
+            const { id } = req.query;
+            console.log(id);
+            const screenings = await Screenings.find({ id: id });
+            if (screenings.length > 0) {
+                res.status(200).json(screenings);
+            } // if (screenings) is an empty array, return 404.
+            else {
+                res.status(404).json({ message: "No screenings found for this movie!" });
+            }
+
+        });
+    } catch (error) {
+        // If error (Database unavailable), return 500.
+        res.status(500).json({ message: "Error getting screenings!" });
+    }
+}
