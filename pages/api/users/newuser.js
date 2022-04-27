c3c501be9b2dd32ed444bc383bf0787191148cf1
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-04-27 14:45:17 +0200

    Update newuser.js

--- pages/api/users/newuser.js
+++ pages/api/users/newuser.js
@@ -1,10 +1,17 @@
 // Route for creating a new user /api/users/newuser.
 
 export default function userHandler(req, res) {
-    if (req.method === 'POST') {
-        console.log(req.body + req.method);
-        res.status(200).send("POST works!");
+    if (req.method === "POST") {
+        if (
+            req.body.name.length > 3 &&
+            req.body.phone.length > 10 &&
+            req.body.email.length > 6 &&
+            req.body.password.length > 6
+        ) {
+            // TODO: Create a new user and send to database. Right now it only sends a message.
+            res.status(200).send({message: "User created!"});
+        } else {
+            res.status(500)}
     } else {
-        res.status(405).send("Method not allowed");
-    }
+        res.status(500)}
 }
