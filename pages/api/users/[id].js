649ccc6fc4170d15ca5d6c4124a40583e548e59f
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-04-27 11:06:22 +0200

    get user info route

---
+++ pages/api/users/[id].js
@@ -0,0 +1,28 @@
+import connectDb from "../../../DB/connectDb";
+import User from "../../../DB/models/users";
+
+// Find a user with id from /api/users/[id].
+export default async function handler(req, res) {
+    try {
+        connectDb().then(async () => {
+            const { id } = req.query;
+            const user = await User.findOne({ id: id });
+            if (user) {
+                // What information to send back.
+                const data = {
+                    id: user.id,
+                    name: user.name,
+                    phone: user.phone,
+                    email: user.email,
+                }
+                res.status(200).json(data);
+            } else {
+                // If no user with ID is found, return 404.
+                res.status(404).json({ message: "No user found!" });
+            }
+        });
+    } catch (error) {
+        // If error (Database unavailable), return 500.
+        res.status(500).json({ message: "Error getting users!" });
+    }
+}
