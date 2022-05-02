1433d89d91e36209d0b1bca654574eec881f6336
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-05-02 11:32:43 +0200

    test

--- pages/api/users/newuser.js
+++ pages/api/users/newuser.js
@@ -1,5 +1,7 @@
-// Route for creating a new user /api/users/newuser.
+import connectDb from "../../../DB/connectDb";
+import User from "../../../DB/models/users";
 
+// Route for creating a new user /api/users/newuser.
 export default function userHandler(req, res) {
     if (req.method === "POST") {
         if (
@@ -8,10 +10,30 @@ export default function userHandler(req, res) {
             req.body.email.length > 6 &&
             req.body.password.length > 6
         ) {
-            // TODO: Create a new user and send to database. Right now it only sends a message.
-            res.status(200).send({message: "User created!"});
+            // Look in the database if email is already in use.
+            connectDb().then(async () => {
+                const user = await User.findOne({ email: req.body.email });
+                if (user) {
+                    res.status(400).json({ message: "An account with that email is already in use!" });
+                } else {
+                    // If email is not in use, create a new user.
+                    const newUser = new User({
+                        name: req.body.name,
+                        phone: req.body.phone,
+                        email: req.body.email,
+                        password: req.body.password,
+                        token: "",
+                        isAdmin: false,
+                        registered: new Date(),
+                    });
+                    await newUser.save();
+                    res.status(200).json({ message: "User created!" });
+                }
+            });
         } else {
-            res.status(500)}
+            res.status(400).json({ message: "Please fill in all fields correctly!", });
+        }
     } else {
-        res.status(500)}
+        res.status(400).json({ message: "Method not allowed!" });
+    }
 }
