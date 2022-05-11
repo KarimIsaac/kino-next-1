fb87d70c2503bd7da508dbccd1d1cb8a31e1d9f7
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-05-11 14:12:41 +0200

    FIXED: ERR: Cannot set headers after...

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,5 +1,6 @@
 //TODO: remove this api an connect to the MongoDB database at Login with ServerSideProps();
 import Cookies from "cookies";
+import { resolveHref } from "next/dist/shared/lib/router/router";
 import { v4 as uuid_V4 } from "uuid";
 import connectDb from "../../DB/connectDb";
 import User from "../../DB/models/users";
@@ -7,27 +8,33 @@ import User from "../../DB/models/users";
 export default function loginChecker(req, res) {
   const cookie = new Cookies(req, res);
   const { userName, password } = req.body;
-  let infromation;
 
-  connectDb().then(async () => {
+  return connectDb().then(async () => {
+    let infromation;
+    const sessionValue = uuid_V4();
     const user = await User.find();
     user.forEach((account) => {
       if (account.name === userName && account.password === password) {
-        const sessionValue = uuid_V4();
         infromation = {
           user: account.name,
           key: sessionValue,
           login: true,
         };
-
-        cookie.set("_session", sessionValue);
-        res.status(200).json(infromation);
-      } else {
-        res.status(401).send({
-          err: true,
-          message: "401: account do not exist!",
-        });
       }
     });
+
+    if (infromation.login) {
+      cookie.set("_session", sessionValue);
+      res.json(infromation);
+      return res.status(200).end();
+    } else {
+      res
+        .status(401)
+        .send({
+          err: true,
+          message: "401: account do not exist!",
+        })
+        .end();
+    }
   });
 }
