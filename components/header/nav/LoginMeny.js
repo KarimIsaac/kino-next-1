dbe73478207cc0fc695c99c97d2a519ef18b401b
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-05-14 12:26:50 +0200

    added sign up/sign out btn

--- components/header/nav/LoginMeny.js
+++ components/header/nav/LoginMeny.js
@@ -1,25 +1,48 @@
 import MenyLink from "./MenyLink";
+import styles from "../../../styles/Nav.module.css";
+
 import { LoginContext } from "../../contexts/loginContext";
 import { useContext } from "react";
 
-export default function LoginMeny({ _class }) {
-  const {userObj} = useContext(LoginContext)
-  return (
-    <div className={_class}>
-      <MenyLink
-        _tag={"/login"}
-        _innerText={"login"}
-        _divClass={"login-meny-link"}
-      />
+export default function LoginMeny() {
+  const { userObj } = useContext(LoginContext);
 
-      {userObj.user !== null 
-      ? <MenyLink
-        _tag={"/my-page"}
-        _innerText={userObj.user}
-        _divClass={"login-meny-link"}
-      />
-    : <a></a>}
-      
+  function signOutUser() {
+    window.cookie = "_session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.reload(false);
+  }
+
+  return (
+    <div className={styles.nav_utilites_containter}>
+      {userObj.user !== null ? (
+        <div className={styles.utilites_link_container}>
+          <MenyLink
+            _tag={"/my-page"}
+            _innerText={userObj.user}
+            _divClass={"login-meny-link"}
+          />
+          <button
+            onClick={() => {
+              signOutUser();
+            }}
+          >
+            sign out
+          </button>
+        </div>
+      ) : (
+        <div className={styles.utilites_link_container}>
+          <MenyLink
+            _tag={"/login"}
+            _innerText={"login"}
+            _divClass={"login-meny-link"}
+          />
+          <MenyLink
+            _tag={"/signup"}
+            _innerText={"sign up"}
+            _divClass={"login-meny-link"}
+          />
+        </div>
+      )}
     </div>
   );
 }
