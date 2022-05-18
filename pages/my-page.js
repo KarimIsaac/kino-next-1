2fae83dcf85913d9d489e46766550860fd6cacc3
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-05-18 13:05:34 +0200

    added working my-page + styling

--- pages/my-page.js
+++ pages/my-page.js
@@ -1,14 +1,46 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { LoginContext } from "../components/contexts/loginContext";
+import style from "../styles/MyPage.module.css";
+import Head from "next/head";
 
-export default function MyPage() {
+export default function MyPage({ session }) {
   const { userObj } = useContext(LoginContext);
 
-  return (
-    <>
-      <p>
-        {userObj.user}
-      </p>
-    </>
-  );
+  if (session === userObj.key) {
+    return (
+      <>
+        <Head>
+          <title>Kino Luleå | {userObj.user}</title>
+          <meta
+            name="description"
+            content="Fictional website for cinema in Luleå"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        { /*========== content ========== */}
+        <div className={style["main-container-content-article-small"]}>
+            <p>Min sida: {userObj.user}</p>
+        </div>
+        <div className={style["my-page"]}>
+          <div>
+            <h2>{userObj.user}</h2>
+            <p>
+              <b>E-mail: </b> {userObj.email}
+            </p>
+            <p>
+              <b>Konto-id: </b>
+              {userObj.id}
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export async function getServerSideProps({ req }) {
+  return {
+    props: { session: req.cookies._session || "" }, // will be passed to the page component as props
+  };
 }
